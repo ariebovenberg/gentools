63081f89a3b5372c5d5dbeb469de39d452111b62
Arie Bovenberg <ariebovenberg@wlnss.com>
2018-01-23 08:48:04 +0100

    fix flake8 exemption

--- gentools/__init__.py
+++ gentools/__init__.py
@@ -1,4 +1,4 @@
 from .core import *  # noqa
 from .types import *  # noqa
-from .__about__ import (__author__, __copyright__, __version__,  #  noqa
+from .__about__ import (__author__, __copyright__, __version__,  # noqa
                         __description__)
